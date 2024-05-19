c70fc443ebbc7bab18d7794ec28b3c6ec47b86c4
Sarlota-Duskova <duskova.s@gmail.com>
2024-05-19 14:06:45 +0200

    with #

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 //import { BrowserRouter as BrowserRouter, Routes, Route } from 'react-router-dom';
-import { HashRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route , useNavigate} from 'react-router-dom';
 
 /* -------------- Pages -------------- */
 import Home from './pages/Home';
@@ -11,16 +11,18 @@ import Pokus from './pages/Pokus';
 //basename="/github-test"
 //"homepage": "https://sarlota-duskova.github.io/github-test",
 export default function App() {
-
+  const navigate = useNavigate();
   return (
     <div>
-      <HashRouter>
+        <button onClick={() => navigate('/')}>Home</button>
+        <button onClick={() => navigate('/team')}>Team</button>
+        <button onClick={() => navigate('/pokus')}>Team</button>
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/team" element={<TeamPage />} />
-          <Route path="/github-test/pokus" element={<Pokus />} />
+          <Route path="/pokus" element={<Pokus />} />
         </Routes>
-      </HashRouter>
+
     </div>
   );
 }
