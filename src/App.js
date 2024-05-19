2d83c58d56edc09912ceae093fe9ea6774a9ba96
Sarlota-Duskova <duskova.s@gmail.com>
2024-05-19 13:33:20 +0200

    still trying

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as BrowserRouter, Routes, Route } from 'react-router-dom'
 /* -------------- Pages -------------- */
 import Home from './pages/Home';
 import TeamPage from './pages/TeamPage';
+import Pokus from './pages/Pokus';
 
 export default function App() {
 
@@ -15,6 +16,7 @@ export default function App() {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/team" element={<TeamPage />} />
+          <Route path="/github-test/pokus" element={<Pokus />} />
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/pages/Pokus.js
@@ -0,0 +1,10 @@
+/* ------------ Important ------------ */
+import React from 'react'
+
+export default function Pokus() {
+    return (
+        <div>
+            <h1>Pokus File because I dont know whats wrong</h1>
+        </div>
+    )
+}
